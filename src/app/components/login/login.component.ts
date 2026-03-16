3bc923c64af8844c614a185555e8dc4c893233b4
Mauricio Chaves <mauricio.chaves.jr@gmail.com>
2026-03-16 15:23:54 -0300

    Criação do login service

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -16,8 +18,25 @@ export class LoginComponent {
   });
 
   private readonly _router = inject(Router);
+  private readonly _loginService = inject(LoginService);
 
   onLogin() {
+    this._loginService.login(
+      this.loginForm.value.username,
+      this.loginForm.value.password)
+      .subscribe({
+        next: (tokenResponse) => {
+          this._router.navigate(['user-infos']);
+        },
+        error: (error: HttpErrorResponse) => {
+          const UNAUTHORIZED_RESPONSE_ERROR = 401;
 
+          if (error.status === UNAUTHORIZED_RESPONSE_ERROR) {
+            this.loginForm.setErrors({ 'invalidCredentials': true });
+          } else {
+            this.loginForm.setErrors({ 'generalCredentialsError': true });
+          }
+        },
+      });
   }
 }
