67350cc05a81a642950b9083e7d4911b90e58062
Mauricio Chaves <mauricio.chaves.jr@gmail.com>
2026-03-18 23:44:09 -0300

    Passando valores para o interceptor

--- src/app/services/create-user.service.ts
+++ src/app/services/create-user.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpContext, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { map } from 'rxjs';
 import { IUserRequest } from '../interfaces/user-request.interface';
 import { IUserCreateResponse } from '../interfaces/user-create-response.interface';
+import { AUTH_TOKEN_ENABLED } from '../interceptors/auth.interceptor';
 
 @Injectable({
   providedIn: 'root',
@@ -13,9 +14,13 @@ export class CreateUserService {
   createUser(newUser: IUserRequest) {
 
     //const headers = new HttpHeaders().set('authorization', 'Bearer ' + localStorage.getItem('token')!);
-
+    const newHeaders = new HttpHeaders().set('useAuth', 'y');
     return this._httpClient.post<IUserCreateResponse>('http://localhost:3000/create-user', 
-      newUser      
+      newUser,
+      {
+        headers: newHeaders,
+        context: new HttpContext().set(AUTH_TOKEN_ENABLED, false),
+      }      
     );
   };
 };
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpContext, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { ILoginResponse } from '../interfaces/login-response.interface';
+import { AUTH_TOKEN_ENABLED } from '../interceptors/auth.interceptor';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +12,16 @@ export class LoginService {
   private readonly _httpClient = inject(HttpClient);
 
   login(username: string, password: string): Observable<ILoginResponse> {
+    const newHeaders = new HttpHeaders().set('useAuth', 'n');
+
     return this._httpClient.post<ILoginResponse>('http://localhost:3000/login', {
       username,
       password,
-    }).pipe(
+    },
+  {
+    headers: newHeaders,
+    context: new HttpContext().set(AUTH_TOKEN_ENABLED, false),
+  }).pipe(
       map((tokenResponse) => {
         localStorage.setItem('token', tokenResponse.token);
 
